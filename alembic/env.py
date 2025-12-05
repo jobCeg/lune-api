5dc7cef2f0e6e6979337b336c2a606cc32a3adc1
Roberto Plancarte <perezplancarter@gmail.com>
2025-12-05 09:36:32 -0600

    Update comments to English in env.py

--- alembic/env.py
+++ alembic/env.py
@@ -5,7 +5,8 @@ from sqlalchemy import engine_from_config, pool
 from alembic import context
 from app.core.db import Base  # Importa tu Base desde app/core/db.py
 
-# Cargar variables de entorno
+# Load environment variables
+
 load_dotenv()
 
 DB_HOST = os.getenv("DB_HOST", "localhost")
@@ -14,18 +15,19 @@ DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_PASS = os.getenv("DB_PASS")
 
-# Construir URL de la base de datos
+# Build database URL
+
 DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
-# Configuración Alembic
+
 config = context.config
 config.set_main_option("sqlalchemy.url", DATABASE_URL)
 
-# Configuración de logging
+
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-# Metadata para autogenerar migraciones
+
 target_metadata = Base.metadata
 
 
